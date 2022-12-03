8054f4e98069894c089fc3ea675dc7a6d0fb41fa
muhammedakb <muhammetakb68@gmail.com>
2022-12-03 20:13:42 +0300

    Debounce callbacks on DOM events in react apps

--- egghead_io/react_optimization_cookbook/src/hooks/useMouse.js
+++ egghead_io/react_optimization_cookbook/src/hooks/useMouse.js
@@ -3,6 +3,10 @@ import debounce from "lodash/debounce";
 
 const useMousePosition = () => {
   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  // normalde her pozisyonu değiştinde anlık sürekli yazdırır
+  // ama lodash'ten debounce fonksiyonu ile sarmalayıp 100ms
+  // parametre verince mousemove event'i bittikten 100ms sonra yazdırır
+  // buda daha performanslı çalışır
   const updateMousePosition = debounce((ev) => {
     setMousePosition({ x: ev.clientX, y: ev.clientY });
   }, 100);
