f123556464535e5f69cde1d391662b04be9c6dc0
muhammedakb <muhammetakb68@gmail.com>
2023-01-08 00:22:43 +0300

    queue data structure in javascript

---
+++ egghead_io/Data-Structures-and-Algorithms-in-JavaScript/queue.js
@@ -0,0 +1,41 @@
+// Queues (kuyruk)
+
+// first in first out (FIFO)
+
+function createQueue() {
+  const queue = [];
+
+  return {
+    // add or enqueue
+    enqueue(item) {
+      queue.unshift(item);
+    },
+    // remove or enqueue
+    dequeue() {
+      return queue.pop();
+    },
+    // peek
+    peek() {
+      return queue[queue.length - 1];
+    },
+    // length
+    get length() {
+      return queue.length;
+    },
+    // isEmpty
+    isEmpty() {
+      return queue.length === 0;
+    },
+  };
+}
+
+const q = createQueue();
+
+q.enqueue('zortinaytii');
+q.enqueue('ilaaaaaa');
+q.enqueue('babaciiiiiii');
+
+q.dequeue();
+q.dequeue();
+q.dequeue();
+console.log(q.isEmpty());
