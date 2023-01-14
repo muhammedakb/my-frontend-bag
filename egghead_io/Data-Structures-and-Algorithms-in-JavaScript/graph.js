aaaa0dc797abb5faac2bcebd545bbbd879d2ca10
muhammedakb <muhammetakb68@gmail.com>
2023-01-15 00:16:37 +0300

    JavaScript Graph Data Structure

---
+++ egghead_io/Data-Structures-and-Algorithms-in-JavaScript/graph.js
@@ -0,0 +1,77 @@
+// Graphs
+
+// A collection of nodes also known as vertices (Köşeler olarak da bilinen bir düğüm koleksiyonu)
+// Nodes may point to ohter nodes, known as edges (Düğümler, kenarlar olarak bilinen diğer düğümlere işaret edebilir.)
+
+function createNode(key) {
+  const neighbors = [];
+
+  return {
+    key,
+    neighbors,
+    addNeighbor(node) {
+      neighbors.push(node);
+    },
+  };
+}
+
+function createGraph(directed = false) {
+  const nodes = [];
+  const edges = [];
+
+  return {
+    directed,
+    nodes,
+    edges,
+    addNode(key) {
+      nodes.push(createNode(key));
+    },
+    getNode(key) {
+      return nodes.find((node) => node.key === key);
+    },
+    addEdge(node1key, node2key) {
+      const node1 = this.getNode(node1key);
+      const node2 = this.getNode(node2key);
+
+      node1.addNeighbor(node2);
+      edges.push(`${node1key}-${node2key}`);
+
+      if (!directed) {
+        node2.addNeighbor(node1);
+      }
+    },
+    print() {
+      return nodes
+        .map(({ key, neighbors }) => {
+          let result = key;
+
+          if (neighbors.length) {
+            result += ` => ${neighbors
+              .map((neighbor) => neighbor.key)
+              .join(' ')}`;
+          }
+
+          return result;
+        })
+        .join('\n');
+    },
+  };
+}
+
+const graph = createGraph(true);
+
+graph.addNode('Kyle');
+graph.addNode('Anna');
+graph.addNode('Krios');
+graph.addNode('Tali');
+
+graph.addEdge('Kyle', 'Anna');
+graph.addEdge('Anna', 'Kyle');
+graph.addEdge('Kyle', 'Krios');
+graph.addEdge('Kyle', 'Tali');
+graph.addEdge('Anna', 'Krios');
+graph.addEdge('Anna', 'Tali');
+graph.addEdge('Krios', 'Anna');
+graph.addEdge('Tali', 'Kyle');
+
+console.log(graph.print());
