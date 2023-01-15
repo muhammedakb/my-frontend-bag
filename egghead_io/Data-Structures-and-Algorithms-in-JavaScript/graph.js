935c25e5823e9df5137d3d34e20db2c15260814c
muhammedakb <muhammetakb68@gmail.com>
2023-01-15 21:13:20 +0300

    Breadth First JavaScript Search Algorithm for Graphs

--- egghead_io/Data-Structures-and-Algorithms-in-JavaScript/graph.js
+++ egghead_io/Data-Structures-and-Algorithms-in-JavaScript/graph.js
@@ -1,3 +1,5 @@
+import { createQueue } from './queue.js';
+
 // Graphs
 
 // A collection of nodes also known as vertices (Köşeler olarak da bilinen bir düğüm koleksiyonu)
@@ -55,23 +57,73 @@ function createGraph(directed = false) {
         })
         .join('\n');
     },
+    // Breadth First Search
+    breadthFirstSearch(startingNodeKey, visitFn) {
+      const startingNode = this.getNode(startingNodeKey);
+      const visited = nodes.reduce((acc, node) => {
+        acc[node.key] = false;
+        return acc;
+      }, {});
+
+      const queue = createQueue();
+      queue.enqueue(startingNode);
+
+      while (!queue.isEmpty()) {
+        const currentNode = queue.dequeue();
+
+        if (!visited[currentNode.key]) {
+          visitFn(currentNode);
+          visited[currentNode.key] = true;
+        }
+
+        currentNode.neighbors.forEach((node) => {
+          if (!visited[node.key]) {
+            queue.enqueue(node);
+          }
+        });
+      }
+    },
   };
 }
 
 const graph = createGraph(true);
 
-graph.addNode('Kyle');
-graph.addNode('Anna');
-graph.addNode('Krios');
-graph.addNode('Tali');
+// graph.addNode('Kyle');
+// graph.addNode('Anna');
+// graph.addNode('Krios');
+// graph.addNode('Tali');
 
-graph.addEdge('Kyle', 'Anna');
-graph.addEdge('Anna', 'Kyle');
-graph.addEdge('Kyle', 'Krios');
-graph.addEdge('Kyle', 'Tali');
-graph.addEdge('Anna', 'Krios');
-graph.addEdge('Anna', 'Tali');
-graph.addEdge('Krios', 'Anna');
-graph.addEdge('Tali', 'Kyle');
+// graph.addEdge('Kyle', 'Anna');
+// graph.addEdge('Anna', 'Kyle');
+// graph.addEdge('Kyle', 'Krios');
+// graph.addEdge('Kyle', 'Tali');
+// graph.addEdge('Anna', 'Krios');
+// graph.addEdge('Anna', 'Tali');
+// graph.addEdge('Krios', 'Anna');
+// graph.addEdge('Tali', 'Kyle');
 
-console.log(graph.print());
+// console.log(graph.print());
+
+const nodes = ['a', 'b', 'c', 'd', 'e', 'f'];
+const edges = [
+  ['a', 'b'],
+  ['a', 'e'],
+  ['a', 'f'],
+  ['b', 'd'],
+  ['b', 'e'],
+  ['c', 'b'],
+  ['d', 'c'],
+  ['d', 'e'],
+];
+
+nodes.forEach((node) => {
+  graph.addNode(node);
+});
+
+edges.forEach((nodes) => {
+  graph.addEdge(...nodes);
+});
+
+graph.breadthFirstSearch('a', (node) => {
+  console.log(node.key);
+});
