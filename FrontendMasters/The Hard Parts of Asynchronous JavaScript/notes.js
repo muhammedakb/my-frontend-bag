bb96346a9fdf7854bf3660559d471ab69e34ab8f
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-03-31 01:29:10 +0300

    calling the outside world

--- FrontendMasters/The Hard Parts of Asynchronous JavaScript/notes.js
+++ FrontendMasters/The Hard Parts of Asynchronous JavaScript/notes.js
@@ -1,1 +1,34 @@
-console.log('hey');
+function printHello() {
+  console.log('Hello');
+}
+
+function printHi() {}
+
+setTimeout(printHello, 0);
+
+printHi();
+
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+console.log('me first');
+
+// setTimeout -> web api'nin özelliği javascriptin değil !
+
+// 0 ms değerini geçsek bile, alttaki bütün işlemler gerçekleştikten sonra setTimeout içerisindeki fonksiyon çalıştırılır!
+// Bu javascript'in single thread bir dil olmasından kaynaklı.
