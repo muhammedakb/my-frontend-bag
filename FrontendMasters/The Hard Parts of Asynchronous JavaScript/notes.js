db6ac2d0865b7d3c31bc51de844ddb11a3034a59
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-04-18 23:37:20 +0300

    async description

--- FrontendMasters/The Hard Parts of Asynchronous JavaScript/notes.js
+++ FrontendMasters/The Hard Parts of Asynchronous JavaScript/notes.js
@@ -32,3 +32,9 @@ console.log('me first');
 
 // 0 ms değerini geçsek bile, alttaki bütün işlemler gerçekleştikten sonra setTimeout içerisindeki fonksiyon çalıştırılır!
 // Bu javascript'in single thread bir dil olmasından kaynaklı.
+
+// Promise'ler, web API'leri, the callback ve microtask queue'ler ve event loop,
+// eylemlerimizi "iş" (API isteği, zamanlayıcı vb.) tamamlanana kadar ertelememize ve
+// bu arada kodumuzu satır satır çalıştırmaya devam etmemize olanak tanır.
+
+// Asenkron javascript, modern web'in omurgasıdır - hızlı "engellenmeyen" oluşturmamıza olanak tanır
