d1e0930afaeff3c8e740739e8d44fd1f299cae26
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-04-27 18:22:26 +0300

    generator example with parameter

--- FrontendMasters/The Hard Parts of Asynchronous JavaScript/generator.js
+++ FrontendMasters/The Hard Parts of Asynchronous JavaScript/generator.js
@@ -22,3 +22,21 @@ console.log(generatorObj.next());
 // Bir generator objesini yalnızca bir kere iterate edebiliyoruz.
 // Bir kere daha next metodunu çağırırsak -> Sadece sonsuza kadar undefined döner.
 // Eğer tekrar iterate etmek istiyorsanız, yeni bir generator objesi oluşturmanız gerekecek!
+
+// ------------------------------------------------------------------------------------------
+
+function* createFlow() {
+  const num = 10;
+  const newNum = yield num;
+  yield 5 + newNum;
+  yield 6;
+}
+
+const returnNextElement = createFlow();
+const element1 = returnNextElement.next();
+const element2 = returnNextElement.next(2); // burada geçilen parametre yield ifadesinin sağındaki değere atanır
+const element3 = returnNextElement.next();
+
+console.log('element1', element1); // 10
+console.log('element2', element2); // 5 + 2 = 7
+console.log('element3', element3); // 6
