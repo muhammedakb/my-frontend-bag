77a1044470aa52c49aef1407f699c899bb31fb4d
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-06-13 16:58:37 +0300

    closure

--- FrontendMasters/JavaScript: The Hard Parts, v2/closure.js
+++ FrontendMasters/JavaScript: The Hard Parts, v2/closure.js
@@ -10,3 +10,15 @@ function outer() {
 const myNewFunction = outer();
 myNewFunction(); //counter:  1
 myNewFunction(); //counter:  2
+
+const anotherFunction = outer();
+anotherFunction(); //counter:  1
+anotherFunction(); //counter:  2
+
+/* Closure fonksiyonlarımıza kalıcı bellekler ve profesyonel kod yazmak için yepyeni bir araç seti sunar.
+
+Helper Functions: 'once' ve 'memoize' gibi günlük profesyonel helper fonksiyonlar
+Iterators & Generators: JavaScript'te verileri işleme konusunda en modern kalıpları elde etmek için lexical scope ve closure kullanan
+Module Pattern: Global namespace'i kirletmeden bir uygulamanın ömrü boyunca state'i koruyun
+Asynchronous JavaScript: Callback'ler ve Promise'ler, asenkron bir ortamda state'i korumak için closure'a güvenir.
+*/
